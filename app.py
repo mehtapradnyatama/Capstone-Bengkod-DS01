603d9bd1b4c8ff9da0f622eecb3faccca4dbcd34
mehtapradnyatama <mehtapradnyatama@gmail.com>
2025-06-14 17:35:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,9 +8,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
-import plotly.express as px
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
 import os
 from sklearn.ensemble import GradientBoostingClassifier
 import warnings
